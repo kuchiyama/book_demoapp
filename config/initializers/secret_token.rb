af424cb7e47f0f4819efb08e2a8be881d6fc8982
kuchiyama <wiccazuyosi@gmail.com>
2015-06-20 19:36:29 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookDemoapp::Application.config.secret_token = '8596e05a3da9ab8a2c89c4b09fb1c5d660b240ec2dcd2d13f098d2f2be4bc586396cbd8c22a7817c0e2daec8a5004c10fc399988b0afcc3b754ee4604b374a0b'
